fb02c24e456441049fd0e59c183e8acbb64bc55b
Krishnakant upadhyay <84394159+Krishna1305@users.noreply.github.com>
2021-08-29 12:38:09 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -1,1 +1,26 @@
+#include<stdio.h>>
 
+// Author- Krishnaaa
+
+long power(int a, int b)
+{
+    long pow =1;
+    while (b>0)
+    {
+        pow = pow*a;
+        b--;
+    }
+    return (pow);
+}
+int main()
+{
+    int a,b;
+    long result;
+    printf("Enter number \n");
+    scanf("%d",&a);
+    printf("Enter power to be raised\n");
+    scanf("%d",&b);
+    result = power(a,b);
+    printf("%d raised to power %d is equal to %ld",a,b,result);
+    return 0;
+}
